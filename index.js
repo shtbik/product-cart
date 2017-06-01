ff422d2fe3348ed4c245837db691952a94628056
Alexander Shtykov <aashtykov@dasreda.ru>
2017-06-01 20:28:19 +0300

    change index.js

--- index.js
+++ index.js
@@ -39,6 +39,6 @@ app.post('/api/get', function(req, res) {
 	res.json(status);
 })
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 5000, function(){
   console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
